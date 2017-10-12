1190e6c98fd0e3dc1817292353b5a8e430649e17
Jóhann Ísaksson <Johanneinar95@gmail.com>
2017-10-12 16:25:01 +0000

    Beinagrind sett upp

---
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -0,0 +1,20 @@
+package is.ru.stringcalculator;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+ 
+ public class CalculatorTest 
+{
+	public static void main(String args[]) 
+	{
+
+	}
+ 
+    /*
+ 	@Test
+ 	public static void testEmptyString() 
+    {
+    	assertEquals(0,Calculator.add(""));
+    }
+    */
+}
