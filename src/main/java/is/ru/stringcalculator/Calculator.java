6bcb34b5a1083a37ae109725b03bba573953b11c
Jóhann Ísaksson <Johanneinar95@gmail.com>
2017-10-12 20:50:51 +0000

    refactored sum

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -9,18 +9,26 @@ public class Calculator {
  		else{
  			if(text.contains(",")){
  				String numbers[] = text.split(",");
- 				int total = 0;
- 				for(String number : numbers){
- 					total +=toInt(number);
- 				}
- 				return total;
+ 				return sum(numbers);
  			}
  			return 1;
  		}
  	}
 
- 	private static int toInt(String number){
+ 	private static int toInt(String number)
+ 	{
  		return Integer.parseInt(number);
  	}
+
+ 	private static int sum(String [] numbers)
+ 	{
+		int total = 0;
+ 		for(String number : numbers){
+ 			total +=toInt(number);
+ 		}
+ 		return total;
+
+ 	}
+
 
  }
